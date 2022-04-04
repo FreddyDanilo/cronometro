6d02f2defbd2203d5f8ef2780a654fde250367e7
Freddy Danilo <fredduddo@gmail.com>
2022-04-04 17:36:32 +0100

    cronometro

---
+++ app.js
@@ -0,0 +1,67 @@
+const tempo = document.querySelector("div.cronometro div.cronometro-tempo span")
+const btnIniciar = document.querySelector("#iniciar")
+const btnPausar = document.querySelector("#pausar")
+const btnParar = document.querySelector("#parar")
+
+let segundos = 0, minutos = 0, horas = 0
+let intervalo
+
+window.onload = ()=>{
+    btnPausar.setAttribute("disabled", "true")
+    btnParar.setAttribute("disabled", "true")
+}
+
+const renderizar = ()=>{
+    
+    segundos++
+    while(segundos === 60){
+        segundos = 0
+        minutos++
+        while(minutos === 60){
+            minutos = 0
+            horas++
+            while(horas === 24){
+                alert("Ja se passaram 24 horas")
+                horas = 0, minutos = 0, segundos = 0 
+            }
+
+        }
+    }
+
+    tempo.innerHTML = ("0" + horas).slice(-2) + ":" + ("0" + minutos).slice(-2) + ":" + ("0" + segundos).slice(-2)
+
+}
+
+const iniciar = ()=>{
+    intervalo = setInterval(() => {
+        renderizar()
+    }, 1000);
+}
+
+const pausar = ()=>{
+    clearInterval(intervalo)
+}
+
+btnIniciar.onclick = ()=>{
+    iniciar()
+    btnIniciar.setAttribute("disabled", "true")
+    btnPausar.removeAttribute("disabled")
+    btnParar.removeAttribute("disabled")
+}
+
+btnPausar.onclick = ()=>{
+    pausar()
+    btnPausar.setAttribute("disabled", "true")
+    btnIniciar.removeAttribute("disabled", "true")
+    btnParar.removeAttribute("disabled", "true")
+    btnIniciar.innerHTML = "Continuar"
+}
+
+btnParar.onclick = ()=>{
+    segundos = -1, minutos = 0, horas = 0
+    pausar()
+    btnParar.setAttribute("disabled", "true")
+    btnIniciar.removeAttribute("disabled", "true")
+    btnPausar.setAttribute("disabled", "true")
+    btnIniciar.innerHTML = "Reiniciar"
+}
